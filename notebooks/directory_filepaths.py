7ae30cf220a696b985b344b023844c0bea7e5346
Nick Malleson <ID+USERNAME@users.noreply.github.com>
2026-03-09 16:10:24 +0000

    checked script 5 and wrote out cluster map for the paper

--- notebooks/directory_filepaths.py
+++ notebooks/directory_filepaths.py
@@ -7,4 +7,4 @@ imd_file =   os.path.join("..", "data", "raw", "imd", "File_2_-_IoD2025_Domains_
 
 h5_filename = os.path.join("..", "data", "processed", "street_data.h5")
 
-outputs_dir = os.path.join(data_dir, "..", "outputs")
+outputs_dir = os.path.join( "..", "outputs")
