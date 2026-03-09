2b3206b0436b8399ae11223fc2d16ba1f1139b26
Nick Malleson <ID+USERNAME@users.noreply.github.com>
2026-03-09 15:59:32 +0000

    tidied up and added new dir for outputs

--- notebooks/directory_filepaths.py
+++ notebooks/directory_filepaths.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
 
-data_dir =   os.path.join("../data/processed/")
-lsoas_file = os.path.join("../data/raw/spatial/LSOAs_2021/LSOA_2021_EW_BSC_V4.shp")
-imd_file =   os.path.join("../data/raw/imd/File_2_-_IoD2025_Domains_of_Deprivation.xlsx")
+data_dir =   os.path.join("..", "data", "processed")
+lsoas_file = os.path.join("..", "data", "raw", "spatial", "LSOAs_2021", "LSOA_2021_EW_BSC_V4.shp")
+imd_file =   os.path.join("..", "data", "raw", "imd", "File_2_-_IoD2025_Domains_of_Deprivation.xlsx")
 
-h5_filename = os.path.join("../data/processed/street_data.h5")
+h5_filename = os.path.join("..", "data", "processed", "street_data.h5")
+
+outputs_dir = os.path.join(data_dir, "..", "outputs")
